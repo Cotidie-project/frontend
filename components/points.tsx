65080fb173ec8738b8fdd0e991642690ec3e97a7
devnull03 <56480041+devnull03@users.noreply.github.com>
2022-04-09 23:37:26 +0530

    feat: modified styling of points component and index page

--- components/points.tsx
+++ components/points.tsx
@@ -1,28 +1,29 @@
 import { NextPage } from "next";
+import { useState } from "react";
 
 interface Props {
     totalPoints: number;
     currentPoints: number;
 }
 const ProgressBar: NextPage<Props> = ({ totalPoints, currentPoints }) => {
+    const [points, setPoints] = useState((currentPoints * 100) / totalPoints);
     return (
-        <div className="flex flex-col p-10 gap-y-5">
-            <p className="text-3xl font-bold">Points</p>
-            <div className="flex flex-row gap-4" id="progress-container">
+        <div className="flex flex-col p-10 gap-y-1">
+            <div className="flex justify-between mb-1">
+                <span className="text-xl font-medium text-blue-700 dark:text-white">
+                    Points
+                </span>
+                <span className="text-sm font-medium text-blue-700 dark:text-white">
+                    {`${points.toFixed(2)}%`}
+                </span>
+            </div>
+            <div className="w-full bg-gray-200 rounded-full h-4 dark:bg-gray-700">
                 <div
-                    className="bg-gray-300 rounded-xl w-full"
-                    id="progress-bar"
-                >
-                    <div
-                        className="bg-green-500 rounded-l-xl h-full"
-                        style={{
-                            width: `${(currentPoints * 100) / totalPoints}%`,
-                        }}
-                    ></div>
-                </div>
-                <p className=" text-base font-semibold">
-                    {currentPoints}/{totalPoints}
-                </p>
+                    className="bg-blue-600 h-4 rounded-full"
+                    style={{
+                        width: `${points}%`,
+                    }}
+                ></div>
             </div>
         </div>
     );
--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -10,15 +10,18 @@ const Home: NextPage = () => {
     return (
         <main className=" bg-background text-white min-h-screen">
             <Navbar />
-            <div className="flex flex-row min-h-full">
-                <div className="flex flex-col w-3/4 m-10 border rounded-xl">
-                    <div className="" id="points area">
-                        <Points
-                            totalPoints={totalPoints}
-                            currentPoints={currentPoints}
-                        />
+            <div className="flex flex-row h-full">
+                <div className="flex flex-col w-3/4 m-10 rounded-lg border border-gray-200 shadow-md dark:bg-inherit dark:border-gray-700">
+                    <Points
+                        totalPoints={totalPoints}
+                        currentPoints={currentPoints}
+                    />
+                    <div className="flex flex-col p-10 gap-4 relative" id="task-area">
+                        <p className="font-semibold text-3xl">Tasks</p>
+                        <div className=" rounded-lg border border-gray-200 shadow-inner dark:bg-inherit dark:border-gray-700">
+                            {}
+                        </div>
                     </div>
-                    <div className="" id="task-area"></div>
                 </div>
                 <div className="flex flex-col w-1/4"> </div>
             </div>
