0c0a167ee647ee1a472374cda7706e25b5c247fe
devnull03 <56480041+devnull03@users.noreply.github.com>
2022-04-10 23:14:07 +0530

    feat(points): dynamic progress bar
    added functionality for changing state of progress bar when the points change

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Navbar from "../../components/navbar";
 import Points from "../../components/points";
 import Task, { taskProps } from "../../components/taskcard";
@@ -7,6 +7,7 @@ import Task, { taskProps } from "../../components/taskcard";
 const Home: NextPage = () => {
     const [totalPoints, setTotalPoints] = useState(100);
     const [currentPoints, setCurrentPoints] = useState(69);
+    const taskAreaRef = useRef<HTMLDivElement>(null);
 
     // test data
     const [tasks, setTasks] = useState<taskProps[]>([
@@ -46,6 +47,7 @@ const Home: NextPage = () => {
         <main className=" bg-background text-white min-h-screen">
             <Navbar />
             <div className="flex flex-row h-full">
+                <button></button>
                 <div
                     className="grid grid-flow-row w-3/4 m-10 absolute top-16 bottom-0 rounded-lg border border-gray-200 shadow-md dark:bg-[#262633] dark:border-gray-700"
                     style={{ gridTemplateRows: "20% 80%" }}
@@ -60,15 +62,35 @@ const Home: NextPage = () => {
                         id="task-area"
                     >
                         <div className="font-semibold text-3xl">Tasks</div>
-                        <div className="flex flex-col p-5 pr-3 gap-5 min-h-full overflow-x-clip overflow-y-scroll shadow-inner sm:rounded-lg bg-[#20202d]">
+                        <div
+                            className="customScroll flex flex-col p-5 pr-3 gap-5 min-h-full overflow-x-clip overflow-y-scroll shadow-inner sm:rounded-lg bg-[#20202d]"
+                            ref={taskAreaRef}
+                            onWheel={(e) => {
+                                taskAreaRef.current?.classList.remove(
+                                    "customScroll"
+                                );
+                                setTimeout(() => {
+                                    taskAreaRef.current?.classList.add(
+                                        "customScroll"
+                                    );
+                                }, 500);
+                            }}
+                        >
                             {tasks.map((task, index) => (
-                                <Task
-                                    title={task.title}
-                                    time={task.time}
-                                    description={task.description}
-                                    completed={task.completed}
+                                <div
+                                    onClick={() => {
+                                        setCurrentPoints(currentPoints + 10);
+                                        console.log("clicked");
+                                    }}
                                     key={index}
-                                />
+                                >
+                                    <Task
+                                        title={task.title}
+                                        time={task.time}
+                                        description={task.description}
+                                        completed={task.completed}
+                                    />
+                                </div>
                             ))}
                         </div>
                     </div>
