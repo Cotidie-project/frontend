7c95439f9b79cf20ee6eab57929e1a9c7d17718e
devnull03 <56480041+devnull03@users.noreply.github.com>
2022-04-11 14:11:18 +0530

    refactor: html property name change

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,11 +10,33 @@ import screenshot from "../public/screenshot.png";
 
 const Home: NextPage = () => {
     const [loggedin, setLoggedin] = useState(false);
-    const [faq, setFaq] = useState<{ title: string; desc: string; btntext: string; btnhref: string; }[]>([
-        { title: "What does Cotidie mean?", desc: "Cotidie is Latin for Daily! That's all it is!", btntext: "Get Started", btnhref: "#"},
-        { title: "Why is this free?", desc: "Cotidie was made as a project for Timathon, a Code Jam, so it is a 100% free and open source! So what are you waiting for, get started!", btntext: "Get Started", btnhref: "#" },
-        { title: "How does it work?", desc: "Cotidie's algorithm is simple. You give it a list of tasks, the day, and a list of your breaks. Cotidie carefully analyses and plans out you day for you. You can check the complete algorithm on our GitHub!", btntext: "GitHub", btnhref: "https://github.com/Cotidie-project/backend" },
-        { title: "How is it different from other day planners?", desc: "Cotidie isn't yet another day planner! It's your daily helper!", btntext: "What are you waiting for?", btnhref: "#" },
+    const [faq, setFaq] = useState<
+        { title: string; desc: string; btntext: string; btnhref: string }[]
+    >([
+        {
+            title: "What does Cotidie mean?",
+            desc: "Cotidie is Latin for Daily! That's all it is!",
+            btntext: "Get Started",
+            btnhref: "#",
+        },
+        {
+            title: "Why is this free?",
+            desc: "Cotidie was made as a project for Timathon, a Code Jam, so it is a 100% free and open source! So what are you waiting for, get started!",
+            btntext: "Get Started",
+            btnhref: "#",
+        },
+        {
+            title: "How does it work?",
+            desc: "Cotidie's algorithm is simple. You give it a list of tasks, the day, and a list of your breaks. Cotidie carefully analyses and plans out you day for you. You can check the complete algorithm on our GitHub!",
+            btntext: "GitHub",
+            btnhref: "https://github.com/Cotidie-project/backend",
+        },
+        {
+            title: "How is it different from other day planners?",
+            desc: "Cotidie isn't yet another day planner! It's your daily helper!",
+            btntext: "What are you waiting for?",
+            btnhref: "#",
+        },
     ]);
     const router = useRouter();
     useEffect(() => {
@@ -362,41 +384,85 @@ const Home: NextPage = () => {
                     <div className="md:flex md:justify-between">
                         <div className="mb-6 md:mb-0">
                             <a href="#" className="flex items-center">
-                                <Image src={logo} className="mr-3 h-8" height="40" width="40" alt="Cotidie Logo" />
-                                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">&nbsp;&nbsp;Cotidie</span>
+                                <Image
+                                    src={logo}
+                                    className="mr-3 h-8"
+                                    height="40"
+                                    width="40"
+                                    alt="Cotidie Logo"
+                                />
+                                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                                    &nbsp;&nbsp;Cotidie
+                                </span>
                             </a>
                         </div>
                         <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                             <div>
-                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                                    Resources
+                                </h2>
                                 <ul className="text-gray-600 dark:text-gray-400">
                                     <li className="mb-4">
-                                        <a href="https://nextjs.org" className="hover:underline">NextJS</a>
+                                        <a
+                                            href="https://nextjs.org"
+                                            className="hover:underline"
+                                        >
+                                            NextJS
+                                        </a>
                                     </li>
                                     <li>
-                                        <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
+                                        <a
+                                            href="https://tailwindcss.com/"
+                                            className="hover:underline"
+                                        >
+                                            Tailwind CSS
+                                        </a>
                                     </li>
                                 </ul>
                             </div>
                             <div>
-                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
+                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                                    Follow us
+                                </h2>
                                 <ul className="text-gray-600 dark:text-gray-400">
                                     <li className="mb-4">
-                                        <a href="https://github.com/Cotidie-project" className="hover:underline ">Github</a>
+                                        <a
+                                            href="https://github.com/Cotidie-project"
+                                            className="hover:underline "
+                                        >
+                                            Github
+                                        </a>
                                     </li>
                                     <li>
-                                        <a href="https://discord.gg/KYH4Y8aktx" className="hover:underline">Discord</a>
+                                        <a
+                                            href="https://discord.gg/KYH4Y8aktx"
+                                            className="hover:underline"
+                                        >
+                                            Discord
+                                        </a>
                                     </li>
                                 </ul>
                             </div>
                             <div>
-                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Code Jam</h2>
+                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                                    Code Jam
+                                </h2>
                                 <ul className="text-gray-600 dark:text-gray-400">
                                     <li className="mb-4">
-                                        <a href="https://twtcodejam.net/timathon/" className="hover:underline">Timathon</a>
+                                        <a
+                                            href="https://twtcodejam.net/timathon/"
+                                            className="hover:underline"
+                                        >
+                                            Timathon
+                                        </a>
                                     </li>
                                     <li>
-                                        <a href="https://twtcodejam.net/timathon/team/1424" className="hover:underline">Our Team</a>
+                                        <a
+                                            href="https://twtcodejam.net/timathon/team/1424"
+                                            className="hover:underline"
+                                        >
+                                            Our Team
+                                        </a>
                                     </li>
                                 </ul>
                             </div>
@@ -404,11 +470,30 @@ const Home: NextPage = () => {
                     </div>
                     <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                     <div className="sm:flex sm:items-center sm:justify-between">
-                        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="#" className="hover:underline">Cotidie &amp; Contributors</a>. All Rights Reserved.
+                        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                            © 2022{" "}
+                            <a href="#" className="hover:underline">
+                                Cotidie &amp; Contributors
+                            </a>
+                            . All Rights Reserved.
                         </span>
-                        <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0"> 
-                            <a href="https://github.com/Cotidie-project" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" /></svg>
+                        <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+                            <a
+                                href="https://github.com/Cotidie-project"
+                                className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+                            >
+                                <svg
+                                    className="w-5 h-5"
+                                    fill="currentColor"
+                                    viewBox="0 0 24 24"
+                                    aria-hidden="true"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
                             </a>
                         </div>
                     </div>
@@ -419,5 +504,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-
