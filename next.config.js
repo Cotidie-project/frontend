c9be941c19246cbe5dfb4933cf5974ca7d21285a
devnull03 <56480041+devnull03@users.noreply.github.com>
2022-04-15 22:10:11 +0530

    fix(next.config.js): fetching user avatar from discord

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    env: {
+        api: "http://localhost:5000",
+    },
+    images: {
+        domains: ["cdn.discordapp.com"],
+    },
 };
 
 module.exports = nextConfig;
