2499584c2466faddd69b4d7785fdaf1b0912f44b
devnull03 <56480041+devnull03@users.noreply.github.com>
2022-04-15 22:08:52 +0530

    feat: fetching and saving user data on login

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,18 +1,59 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
-import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Navbar from "../../components/navbar";
 import Points from "../../components/points";
-import Task, { taskProps } from "../../components/taskcard";
+import Task from "../../components/taskcard";
 import icon from "../../public/icon.ico";
+import Image from "next/image";
+import { serialize } from "cookie";
 
-const Home: NextPage = () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const token = context.req.cookies["token"];
+    const newLogin = context.query["new"];
+
+    if (token) {
+        if (newLogin) {
+            const response = await fetch(`${process.env.api}/auth/user`, {
+                headers: {
+                    "Discord-Token": token,
+                },
+            });
+            const data = await response.json();
+
+            // const tasks = await fetch(`${process.env.api}/user/tasks`, {
+            //     headers: {
+            //         "Discord-Token": token,
+            //     },
+            // });
+            context.res.setHeader("Set-Cookie", [
+                serialize("user", JSON.stringify(data), {
+                    maxAge: 30 * 24 * 60 * 60,
+                    path: "/",
+                    sameSite: "strict",
+                }),
+            ]);
+            console.log(context.req.cookies);
+            return { props: { token, user: data } };
+        }
+        const data = JSON.parse(context.req.cookies["user"]);
+        return { props: { token, user: data } };
+    }
+
+    return {
+        props: {},
+    };
+};
+
+const Home: NextPage<{ user: any }> = ({ user }) => {
+    const [loggedin, setLoggedin] = useState(user ? true : false);
+    console.log(user);
+
     const [totalPoints, setTotalPoints] = useState(100);
     const [currentPoints, setCurrentPoints] = useState(69);
     const taskAreaRef = useRef<HTMLDivElement>(null);
 
     // test data
-
     const [tasks, setTasks] = useState([
         {
             id: 1,
@@ -68,69 +109,91 @@ const Home: NextPage = () => {
                 <link rel="shortcut icon" href={icon.src} type="image/x-icon" />
             </Head>
             <main className="bg-background text-white">
-                <Navbar />
-                <div
-                    className="grid grid-cols-2 p-10 gap-10"
-                    style={{ gridTemplateColumns: "3fr 1fr" }}
-                >
+                <Navbar loggedin={loggedin} />
+                {!loggedin ? (
+                    <div></div>
+                ) : (
                     <div
-                        className="grid grid-flow-row h-[81.5vh] rounded-lg border border-gray-200 shadow-md dark:bg-[#262633] dark:border-gray-700"
-                        style={{ gridTemplateRows: "20% 80%" }}
+                        className="grid grid-cols-2 p-10 gap-10"
+                        style={{ gridTemplateColumns: "3fr 1fr" }}
                     >
-                        <Points
-                            totalPoints={totalPoints}
-                            currentPoints={currentPoints}
-                        />
                         <div
-                            className="grid grid-rows-2 p-10 pt-2 gap-4 max-h-screen"
-                            style={{ gridTemplateRows: "10% 90%" }}
-                            id="task-area"
+                            className="grid grid-flow-row h-[81.5vh] rounded-lg border border-gray-200 shadow-md dark:bg-[#262633] dark:border-gray-700"
+                            style={{ gridTemplateRows: "20% 80%" }}
                         >
-                            <div className="font-semibold text-3xl">Tasks</div>
+                            <Points
+                                totalPoints={totalPoints}
+                                currentPoints={currentPoints}
+                            />
                             <div
-                                className="customScroll flex flex-col p-5 pr-3 gap-5 overflow-x-clip overflow-y-scroll shadow-inner sm:rounded-lg bg-[#20202d]"
-                                ref={taskAreaRef}
-                                onWheel={(e) => {
-                                    taskAreaRef.current?.classList.remove(
-                                        "customScroll"
-                                    );
-                                    setTimeout(() => {
-                                        taskAreaRef.current?.classList.add(
+                                className="grid grid-rows-2 p-10 pt-2 gap-4 max-h-screen"
+                                style={{ gridTemplateRows: "10% 90%" }}
+                                id="task-area"
+                            >
+                                <div className="font-semibold text-3xl">
+                                    Tasks
+                                </div>
+                                <div
+                                    className="customScroll flex flex-col p-5 pr-3 gap-5 overflow-x-clip overflow-y-scroll shadow-inner sm:rounded-lg bg-[#20202d]"
+                                    ref={taskAreaRef}
+                                    onWheel={(e) => {
+                                        taskAreaRef.current?.classList.remove(
                                             "customScroll"
                                         );
-                                    }, 500);
-                                }}
-                            >
-                                {tasks.length ? (
-                                    tasks.map((task, index) => (
-                                        <Task
-                                            id={task.id}
-                                            title={task.title}
-                                            time={task.time}
-                                            description={task.description}
-                                            setCompleted={setCompleted}
-                                            completedArray={completed}
-                                            points={{
-                                                currentPoints,
-                                                setCurrentPoints,
-                                                completionPoints:
-                                                    task.completionPoints,
-                                            }}
-                                            key={task.id}
-                                        />
-                                    ))
-                                ) : (
-                                    <div className="flex items-center justify-center text-center h-full font-thin italic text-6xl text-gray-800">
-                                        No Tasks left
-                                    </div>
-                                )}
+                                        setTimeout(() => {
+                                            taskAreaRef.current?.classList.add(
+                                                "customScroll"
+                                            );
+                                        }, 500);
+                                    }}
+                                >
+                                    {tasks.length ? (
+                                        tasks.map((task, index) => (
+                                            <Task
+                                                id={task.id}
+                                                title={task.title}
+                                                time={task.time}
+                                                description={task.description}
+                                                setCompleted={setCompleted}
+                                                completedArray={completed}
+                                                points={{
+                                                    currentPoints,
+                                                    setCurrentPoints,
+                                                    completionPoints:
+                                                        task.completionPoints,
+                                                }}
+                                                key={task.id}
+                                            />
+                                        ))
+                                    ) : (
+                                        <div className="flex items-center justify-center text-center h-full font-thin italic text-6xl text-gray-800">
+                                            No Tasks left
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                        <div
+                            id="user info"
+                            className="flex flex-col h-fit gap-4 p-6 pl-10 rounded-lg border border-gray-200 shadow-md dark:bg-[#262633] dark:border-gray-700"
+                        >
+                            <div className="rounded-full p-0.5 border-2 h-24 w-24 overflow-hidden">
+                                <Image
+                                    src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=256`}
+                                    width="256"
+                                    height="256"
+                                    alt={`${user.username}'s avatar`}
+                                    className="rounded-full m-0 p-0"
+                                />
+                            </div>
+                            {/* <div>{`${user.username}#${user.discriminator}`}</div> */}
+                            <div className="flex flex-row items-baseline">
+                                <p className="text-2xl">{user.username}</p>
+                                <p className="text-sm">{`#${user.discriminator}`}</p>
                             </div>
                         </div>
                     </div>
-                    <div className="flex flex-col h-7 rounded-lg border border-gray-200 shadow-md dark:bg-[#262633] dark:border-gray-700">
-                        {" "}
-                    </div>
-                </div>
+                )}
             </main>
         </>
     );
