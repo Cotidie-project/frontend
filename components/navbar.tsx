a7b79c5e3d5ca2c719ce7292013e2211573aecdf
devnull03 <56480041+devnull03@users.noreply.github.com>
2022-04-18 16:19:40 +0530

    fix(navbar): reeeeeeee

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -33,20 +33,6 @@ const Navbar: NextPage<{ loggedin: boolean }> = ({ loggedin }) => {
                     </a>
                 </Link>
                 <div className="flex md:order-2">
-                    <a
-                        href="#"
-                        className="text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 transition ease-in-out"
-                    >
-                        Plan Day
-                    </a>
-                    <a
-                        href="#"
-                        className="bg-white text-[#07070A] rounded-lg font-medium text-sm px-5 py-2.5 text-center mr-3 transition ease-in-out"
-                    >
-                        Plan Week
-                    </a>
-                </div>
-                <div className="flex md:order-2">
                     <Link href={"/app/plan?type=day"}>
                         <a className="text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 transition ease-in-out">
                             Plan Day
