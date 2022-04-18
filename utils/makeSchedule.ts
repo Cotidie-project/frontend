6d922141aae8df36cd6fcfd9157699901aa1d1e4
devnull03 <56480041+devnull03@users.noreply.github.com>
2022-04-18 20:39:48 +0530

    feat: uhhhhhhhh

--- utils/makeSchedule.ts
+++ utils/makeSchedule.ts
@@ -4,9 +4,13 @@ export default function makeSchedule(
     tasks: string[],
     breaks: Break[],
     planType: PlanType
-): { tasks: Task[]; breaks: Break[] } {
+): { tasks: any[]; breaks: Break[] } {
     return {
-        tasks: [],
-        breaks: [],
+        tasks: tasks.map((task) => ({
+            title: task,
+            time: new Date(200, 0, 1, Math.random() * 24, Math.random() * 60),
+            points: 10,
+        })),
+        breaks: breaks,
     };
 }
